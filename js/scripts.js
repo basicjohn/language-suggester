818e0602ca88a0797b29cf49d02e553c7ccbe3bb
basicjohn <edmondsonj@gmail.com>
2021-03-26 10:25:37 -0700

    fix scripts file linking issues and create initial javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 // Business logic:
-var question1;
-var question2;
-var question3;
-var question4;
-var question5;
+var question1
+var question2
+var question3
+var question4
+var question5
+
+
+
+
 
 // Everything below this line is user interface logic:
 $(document).ready(function () {
+  $("#suggesterQuestions").submit(function (event) {
+    question1 = $("#question1").val();
+    console.log(question1);
+    event.preventDefault();
+  });
+
+
+
+
+
 
 });
