934b2702dd11ce2d385ada3372fe9d0bf40081dc
basicjohn <edmondsonj@gmail.com>
2021-03-26 11:02:31 -0700

    content to quiz results section

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,15 @@ var question5
 $(document).ready(function () {
   $("#suggesterQuestions").submit(function (event) {
     question1 = $("#question1").val();
+    question2 = $("#question2").val();
+    question3 = $("#question3").val();
+    question4 = $("#question4").val();
+    question5 = $("#question5").val();
     console.log(question1);
+    console.log(question2);
+    console.log(question3);
+    console.log(question4);
+    console.log(question5);
     event.preventDefault();
   });
 
