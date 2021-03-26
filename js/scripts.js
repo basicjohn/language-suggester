b350e46ff1fc5deac8c80bb17d576b46c420c913
basicjohn <edmondsonj@gmail.com>
2021-03-26 09:27:42 -0700

    add a div layout ready for implementing scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business logic:
+var question1;
+var question2;
+var question3;
+var question4;
+var question5;
+
+// Everything below this line is user interface logic:
+$(document).ready(function () {
+
+});
